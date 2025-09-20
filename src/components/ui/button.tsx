2f18cdf28df78c497a0d51e2060c7a6e6a9844f4
Chandrabhan Mahato <humanpekka@gmail.com>
2025-09-20 16:10:42 +0000

    hupdate: on hover shining effect is same for both themes for primary but

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90 shine-effect",
+        default: "bg-primary text-primary-foreground hover:bg-primary/90 primary-shine-effect",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
