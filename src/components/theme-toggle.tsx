9f0e08fe97468b0b110085f7e796212129d3909a
Chandrabhan Mahato <humanpekka@gmail.com>
2025-09-20 15:02:51 +0000

    update: Button now says "Theme: {icon}" (_for element <Button>_)

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -19,16 +19,19 @@ export default function ThemeToggle() {
 
   if (!isMounted) {
     return (
-      <Button variant="outline" size="icon" disabled>
-        <Sun className="h-4 w-4" />
+      <Button variant="outline" size="sm" disabled>
+        Theme: <Sun className="ml-2 h-4 w-4" />
       </Button>
     );
   }
 
   return (
-    <Button variant="outline" size="icon" onClick={toggleTheme}>
-      <Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    <Button variant="outline" size="sm" onClick={toggleTheme}>
+      Theme:
+      <span className="relative ml-2 h-4 w-4">
+        <Sun className="h-full w-full rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <Moon className="absolute top-0 left-0 h-full w-full rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      </span>
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
