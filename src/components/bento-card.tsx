4d5611a6a08eb469067a3a6ce1f9f883085858a7
Chandrabhan Mahato <humanpekka@gmail.com>
2025-09-20 14:29:26 +0000

    update: also used the mac os styling

--- src/components/bento-card.tsx
+++ src/components/bento-card.tsx
@@ -18,11 +18,18 @@ export default function BentoCard({ children, className, title, as: Component = 
     >
       {title && (
         <div className="flex items-center gap-2 border-b p-4">
-            {typeof title === 'string' ? (
-                <p className="font-code text-sm font-bold text-muted-foreground">{title}</p>
-            ) : (
-                title
-            )}
+            <div className="flex gap-1.5">
+                <div className="h-2.5 w-2.5 rounded-full bg-[#EC6A5E]"></div>
+                <div className="h-2.5 w-2.5 rounded-full bg-[#F4BF4F]"></div>
+                <div className="h-2.5 w-2.5 rounded-full bg-[#61C554]"></div>
+            </div>
+            <div className="flex-grow flex justify-center">
+                {typeof title === 'string' ? (
+                    <p className="font-code text-sm font-bold text-muted-foreground">{title}</p>
+                ) : (
+                    title
+                )}
+            </div>
         </div>
       )}
       <div className="p-4 md:p-6 flex-1">{children}</div>
