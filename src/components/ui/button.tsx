ad2e2b5edc50e91d78845502d71621121912a29c
Chandrabhan Mahato <humanpekka@gmail.com>
2025-09-20 16:04:42 +0000

    update: added a subtle shining effect for secondary buttons (_for elemen

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background/20 backdrop-blur-sm hover:bg-accent/20 hover:text-accent-foreground",
+          "border border-input bg-background/20 backdrop-blur-sm hover:bg-accent/20 hover:text-accent-foreground shine-effect",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80 shine-effect",
         ghost: "hover:bg-accent/20 hover:text-accent-foreground",
