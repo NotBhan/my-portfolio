110a43cae81fcbe296d0d7377a3a7c9527ea7e71
Chandrabhan Mahato <humanpekka@gmail.com>
2025-09-20 14:33:01 +0000

    update: adjusted the button to fit the layout properly (_for element <Bu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,21 +10,21 @@ import ThemeToggle from '@/components/theme-toggle';
 export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center p-4 sm:p-8 md:p-12 lg:p-16">
-      <div className="absolute top-4 right-4 z-10">
-        <ThemeToggle />
-      </div>
       <div className="w-full max-w-6xl">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
           <div className="md:col-span-3">
             <Stats />
           </div>
-          <div className="md:col-span-2">
+          <div className="md:col-span-1 flex justify-end items-start">
+            <ThemeToggle />
+          </div>
+          <div className="md:col-span-3">
             <Hero />
           </div>
           <div className="md:col-span-1 row-span-2">
             <Testimonials />
           </div>
-          <div className="md:col-span-2">
+          <div className="md:col-span-3">
             <Partners />
           </div>
           <div className="md:col-span-1">
