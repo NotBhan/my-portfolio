d0a73edf5ea613a10d9b7e6be933ee1f0d8e11e6
Chandrabhan Mahato <humanpekka@gmail.com>
2025-09-21 09:15:57 +0000

    update: increased the intenisty of mouse follower for light theme

--- src/components/spotlight.tsx
+++ src/components/spotlight.tsx
@@ -1,9 +1,11 @@
 'use client';
 
+import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
 
 export default function Spotlight() {
   const [position, setPosition] = useState({ x: -200, y: -200 });
+  const { theme } = useTheme();
 
   useEffect(() => {
     const handleMouseMove = (e: MouseEvent) => {
@@ -17,11 +19,14 @@ export default function Spotlight() {
     };
   }, []);
 
+  const spotlightColor =
+    theme === 'light' ? 'rgba(63, 81, 181, 0.2)' : 'rgba(63, 81, 181, 0.1)';
+
   return (
     <div
       className="pointer-events-none fixed inset-0 z-30 transition duration-300"
       style={{
-        background: `radial-gradient(600px at ${position.x}px ${position.y}px, rgba(63, 81, 181, 0.1), transparent 80%)`,
+        background: `radial-gradient(600px at ${position.x}px ${position.y}px, ${spotlightColor}, transparent 80%)`,
       }}
     />
   );
