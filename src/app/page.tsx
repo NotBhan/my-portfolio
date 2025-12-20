4822bed87e1fc0f3921a724fcf36ca39f8be5c7c
Chandrabhan Mahato <humanpekka@gmail.com>
2025-12-20 14:35:47 +0000

    move around teh smaller bento boxes to fill teh remaining gap in mid and

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,6 @@ export default async function Home() {
         </h1>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-        {/* Left Column */}
         <div className="lg:col-span-2 flex flex-col gap-4">
           <div
             className="animate-slide-in-down opacity-0"
@@ -39,58 +38,52 @@ export default async function Home() {
           </div>
           <div
             className="animate-slide-in-up opacity-0"
-            style={{ animationDelay: '300ms', animationFillMode: 'forwards' }}
-          >
-            <Experiences />
-          </div>
-           <div
-            className="animate-slide-in-up opacity-0"
             style={{ animationDelay: '200ms', animationFillMode: 'forwards' }}
           >
             <Projects />
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div className="flex flex-col gap-4">
-              {showCreativeSkills && (
-                <div
-                  className="animate-slide-in-left opacity-0"
-                  style={{ animationDelay: '500ms', animationFillMode: 'forwards' }}
-                >
-                  <CreativeSkills />
-                </div>
-              )}
-              {showActivities && (
-                <div
-                  className="animate-slide-in-left opacity-0"
-                  style={{ animationDelay: '700ms', animationFillMode: 'forwards' }}
-                >
-                  <Activities />
-                </div>
-              )}
-            </div>
-            <div className="flex flex-col gap-4">
-              <div
-                className="animate-slide-in-right opacity-0"
-                style={{ animationDelay: '600ms', animationFillMode: 'forwards' }}
-              >
-                <Testimonials />
-              </div>
-              <div
-                className="animate-slide-in-right opacity-0"
-                style={{ animationDelay: '800ms', animationFillMode: 'forwards' }}
-              >
-                <Contact />
-              </div>
-            </div>
+          <div
+            className="animate-slide-in-up opacity-0"
+            style={{ animationDelay: '300ms', animationFillMode: 'forwards' }}
+          >
+            <Experiences />
           </div>
         </div>
-        {/* Right Column */}
+        
         <div className="lg:col-span-1 flex flex-col gap-4">
           <div
             className="animate-slide-in-up opacity-0"
             style={{ animationDelay: '400ms', animationFillMode: 'forwards' }}
           >
             <Skills />
+          </div>
+          {showCreativeSkills && (
+            <div
+              className="animate-slide-in-left opacity-0"
+              style={{ animationDelay: '500ms', animationFillMode: 'forwards' }}
+            >
+              <CreativeSkills />
+            </div>
+          )}
+          {showActivities && (
+            <div
+              className="animate-slide-in-left opacity-0"
+              style={{ animationDelay: '600ms', animationFillMode: 'forwards' }}
+            >
+              <Activities />
+            </div>
+          )}
+          <div
+            className="animate-slide-in-right opacity-0"
+            style={{ animationDelay: '700ms', animationFillMode: 'forwards' }}
+          >
+            <Testimonials />
+          </div>
+          <div
+            className="animate-slide-in-right opacity-0"
+            style={{ animationDelay: '800ms', animationFillMode: 'forwards' }}
+          >
+            <Contact />
           </div>
           {showAbout && (
             <div
